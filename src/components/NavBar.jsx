c03e9b8d49827c9f89fd90213e0a1fc39920d2a5
Amir <amiiir@gmail.com>
2023-11-03 17:41:13 +0100

    fix: navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,20 +2,20 @@ import { Link } from 'react-router-dom';
 
 const Navbar = () => {
     return (
-        <nav class="navbar navbar-expand-lg bg-body-secondary">
-            <div class="container-fluid">
-                <a class="navbar-brand" href="/">Instacrame</a>
-                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                    <li class="nav-item nav-link">
+        <nav className="navbar navbar-expand-lg bg-body-secondary">
+            <div className="container-fluid">
+                <a className="navbar-brand" href="/">Instacrame</a>
+                <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li className="nav-item nav-link">
                         <Link to="/">Accueil</Link>
                     </li>
-                    <li class="nav-item nav-link">
+                    <li className="nav-item nav-link">
                         <Link to="/register">Inscription</Link>
                     </li>
-                    <li class="nav-item nav-link">
+                    <li className="nav-item nav-link">
                         <Link to="/feed">Fil public</Link>
                     </li>
-                    <li class="nav-item nav-link">
+                    <li className="nav-item nav-link">
                         <Link to="/like">Like</Link>
                     </li>
                 </ul>
