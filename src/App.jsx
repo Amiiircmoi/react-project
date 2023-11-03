75595ec0854e328c2626fb1739710aee82c67ac1
Amir <amiiir@gmail.com>
2023-11-03 16:22:42 +0100

    feat: navbar integration

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,9 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./redux/store";
 
+// Components 
+import Navbar from "./components/NavBar";
+
 // Pages
 import Register from "./pages/Register";
 
@@ -9,6 +12,7 @@ function App() {
   return (
     <div className="App">
       <Router>
+        <Navbar />
         <Provider store={store}>
           <Routes>
             <Route path="/register" element={<Register />} />
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,24 @@
+import { Link } from 'react-router-dom';
+
+const Navbar = () => {
+    return (
+        <nav class="navbar navbar-expand-lg bg-body-secondary">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="/">Instacrame</a>
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item nav-link">
+                        <Link to="/">Accueil</Link>
+                    </li>
+                    <li class="nav-item nav-link">
+                        <Link to="/register">Inscription</Link>
+                    </li>
+                    <li class="nav-item nav-link">
+                        <Link to="/feed">Fil public</Link>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar
